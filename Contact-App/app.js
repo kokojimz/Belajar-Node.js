e2f33e43f75117519321e23593f8da1f1658238c
Reynaldy Marchell <reynalldymarchell03@gmail.com>
2023-09-12 23:05:35 +0700

    Commit Contact App

--- Contact-App/app.js
+++ Contact-App/app.js
@@ -1,11 +1,11 @@
-const {pertanyaan,simpanContact} = require('./contacts');
+// const {pertanyaan,simpanContact} = require('./contacts');
 
-const main = async() => {
-    const nama = await pertanyaan('Masukkan nama anda: ');
-    const noHP = await pertanyaan('Masukkan nomor HP anda: ');
-    const email = await pertanyaan('Masukkan email anda: ');
+// const main = async() => {
+//     const nama = await pertanyaan('Masukkan nama anda: ');
+//     const noHP = await pertanyaan('Masukkan nomor HP anda: ');
+//     const email = await pertanyaan('Masukkan email anda: ');
 
-    simpanContact(nama,noHP,email);
-};
+//     simpanContact(nama,noHP,email);
+// };
 
-main();
+// main();
