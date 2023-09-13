56af0e514a3c03c24f8691803590bd1823d3e204
Reynaldy Marchell <reynalldymarchell03@gmail.com>
2023-09-13 23:57:41 +0700

    commit Contact-App

--- Contact-App/app.js
+++ Contact-App/app.js
@@ -1,5 +1,54 @@
-// const {pertanyaan,simpanContact} = require('./contacts');
+const yargs = require("yargs");
 
+
+yargs.command({
+    command: 'add',
+    describe: 'Menambahkan contact baru',
+    builder: {
+        nama: {
+            describe: "Nama lengkap",
+            demandOption: true,
+            type: 'string'
+        },
+        email: {
+            describe: "Email",
+            demandOption: false,
+            type: 'string'
+        },
+        noHP: {
+            describe: "Nomor Handphone",
+            demandOption: true,
+            type: 'string'
+        }
+    }
+});
+
+yargs.parse();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const {pertanyaan,simpanContact} = require('./contacts');
 // const main = async() => {
 //     const nama = await pertanyaan('Masukkan nama anda: ');
 //     const noHP = await pertanyaan('Masukkan nomor HP anda: ');
