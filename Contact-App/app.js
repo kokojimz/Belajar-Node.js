67359e990e2016eaf20bb4059d2d472efe870d89
Reynaldy Marchell <reynalldymarchell03@gmail.com>
2023-09-15 02:56:55 +0700

    commit Contact-App

--- Contact-App/app.js
+++ Contact-App/app.js
@@ -1,5 +1,5 @@
 const yargs = require("yargs");
-const { simpanContact } = require("./contacts");
+const { simpanContact,listContact,detailContact,deleteContact } = require("./contacts");
 
 
 yargs.command({
@@ -24,6 +24,47 @@ yargs.command({
     },
     handler(argv) {
         simpanContact(argv.nama,argv.noHP,argv.email);
+    }
+}).demandCommand();
+
+//menampilkan daftar semua nama & no HP contact
+yargs.command({
+    command: 'list',
+    describe: 'Menampilkan semua nama & no HP contact',
+    handler() {
+        listContact();
+    }
+});
+
+//menampilkan detail sebuah contact
+yargs.command({
+    command: 'detail',
+    describe: 'Menampilkan detail contact',
+    builder: {
+        nama: {
+            describe: "Nama lengkap",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        detailContact(argv.nama);
+    }
+});
+
+//menghapus data sebuah contact
+yargs.command({
+    command: 'delete',
+    describe: 'Menghapus data contact',
+    builder: {
+        nama: {
+            describe: "Nama lengkap",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        deleteContact(argv.nama);
     }
 });
 
--- Contact-App/contacts.js
+++ Contact-App/contacts.js
@@ -1,6 +1,7 @@
 const fs = require('node:fs');
 const chalk = require('chalk');
 const validator = require('validator');
+const path = require('node:path');
 
 const log = console.log;
 
@@ -16,13 +17,19 @@ if (!fs.existsSync(dataPath)){
     fs.writeFileSync(dataPath,'[]'); 
 }
 
+//read filecontacts.json
+const readFile = (path) => {
+    const file = fs.readFileSync(path,'utf-8');
+    const contacts = JSON.parse(file);
+    return contacts;
+}
+
 const simpanContact = (nama,noHP,email) => {
-    const contac = {nama,noHP,email};
-    const file = fs.readFileSync(dataPath,'utf-8');
-    const contacs = JSON.parse(file);
+    const contact = {nama,noHP,email};
+    const contacts = readFile(dataPath);
 
     //cek duplikat
-    const duplikat = contacs.find((contact) => contact.nama === nama);
+    const duplikat = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase());
     if (duplikat){
         log(chalk.red.inverse.bold(chalk`Nama ${nama} sudah terdaftar,silahkan gunakan nama lain!`));
         return false;
@@ -42,12 +49,46 @@ const simpanContact = (nama,noHP,email) => {
         return false;
     }
 
-    contacs.push(contac);
+    contacts.push(contact);
 
-    fs.writeFile(dataPath,JSON.stringify(contacs), (err) => {
+    fs.writeFile(dataPath,JSON.stringify(contacts), (err) => {
         if (err) log(err);
         log(chalk.green.inverse.bold('The file has been saved!'));
     });
 };
 
-module.exports = {simpanContact};
+const listContact = () => {
+    const contacts = readFile(dataPath);
+    log(chalk.cyan.inverse.bold('Daftar Contact : '));
+    contacts.forEach((element,i) => {
+        log(`${i+1}. nama : ${element.nama}\n   no HP : ${element.noHP}\n`);
+    });
+};
+
+const detailContact = (nama) => {
+    const contacts = readFile(dataPath);
+    const kontak = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase());
+    if (!kontak){
+        log(chalk.red.inverse.bold(`Nama ${nama} tidak ditemukan!`));
+        return false;
+    }
+    log(chalk.cyan.inverse.bold(`Nama : ${kontak.nama}`));
+    log(chalk.cyan.inverse.bold(`Nomor HP : ${kontak.noHP}`));
+    if(kontak.email) log(chalk.cyan.inverse.bold(`Email : ${kontak.email}`));
+};
+
+const deleteContact = (nama) => {
+    const contacts = readFile(dataPath);
+    const newContacts = contacts.filter((contact) => contact.nama.toLowerCase() != nama.toLowerCase());
+
+    if(contacts.length === newContacts.length){
+        log(chalk.red.inverse.bold(`Nama ${nama} tidak ditemukan!`));
+        return false;
+    }
+    fs.writeFile(dataPath,JSON.stringify(newContacts), (err) => {
+        if (err) log(err);
+        log(chalk.green.inverse.bold('The file has been saved!'));
+    });
+};
+
+module.exports = {simpanContact,listContact,detailContact,deleteContact};
